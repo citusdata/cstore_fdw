cc30f20414f1d9e14fccd542b5dd9e83317f0d68
Murat Tuncer <mtuncer@citusdata.com>
2016-05-11 19:59:49 +0300

    Insert and update scripts

---
+++ cstore_fdw--1.5.sql
@@ -0,0 +1,32 @@
+/* cstore_fdw/cstore_fdw--1.5.sql */
+
+-- complain if script is sourced in psql, rather than via CREATE EXTENSION
+\echo Use "CREATE EXTENSION cstore_fdw" to load this file. \quit
+
+CREATE FUNCTION cstore_fdw_handler()
+RETURNS fdw_handler
+AS 'MODULE_PATHNAME'
+LANGUAGE C STRICT;
+
+CREATE FUNCTION cstore_fdw_validator(text[], oid)
+RETURNS void
+AS 'MODULE_PATHNAME'
+LANGUAGE C STRICT;
+
+CREATE FOREIGN DATA WRAPPER cstore_fdw
+HANDLER cstore_fdw_handler
+VALIDATOR cstore_fdw_validator;
+
+CREATE FUNCTION cstore_ddl_event_end_trigger()
+RETURNS event_trigger
+AS 'MODULE_PATHNAME'
+LANGUAGE C STRICT;
+
+CREATE EVENT TRIGGER cstore_ddl_event_end
+ON ddl_command_end
+EXECUTE PROCEDURE cstore_ddl_event_end_trigger();
+
+CREATE FUNCTION cstore_table_size(relation regclass)
+RETURNS bigint
+AS 'MODULE_PATHNAME'
+LANGUAGE C STRICT;
