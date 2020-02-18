3dfb3b1549a48f8eea0ba6cdb1174b7582a52949
Murat Tuncer <mtuncer@users.noreply.github.com>
2020-02-18 13:00:50 +0300

    Fix count(t.*) type queries after column drop (#223)

--- cstore_fdw.c
+++ cstore_fdw.c
@@ -1886,6 +1886,12 @@ ColumnList(RelOptInfo *baserel, Oid foreignTableId)
 	{
 		ListCell *neededColumnCell = NULL;
 		Var *column = NULL;
+		Form_pg_attribute attributeForm =  TupleDescAttr(tupleDescriptor, columnIndex - 1);
+
+		if (attributeForm->attisdropped)
+		{
+			continue;
+		}
 
 		/* look for this column in the needed column list */
 		foreach(neededColumnCell, neededColumnList)
@@ -1898,7 +1904,6 @@ ColumnList(RelOptInfo *baserel, Oid foreignTableId)
 			}
 			else if (neededColumn->varattno == wholeRow)
 			{
-				Form_pg_attribute attributeForm =  TupleDescAttr(tupleDescriptor, columnIndex - 1);
 				Index tableId = neededColumn->varno;
 
 				column = makeVar(tableId, columnIndex, attributeForm->atttypid,
